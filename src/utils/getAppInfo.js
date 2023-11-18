581c82962e4f6a01e48d174212b0da5c02495dad
kento tokura <k-tokura@nkr-group.com>
2023-11-18 15:48:45 +0900

    connect kintone and officestation and make gui

---
+++ src/utils/getAppInfo.js
@@ -0,0 +1,14 @@
+module.exports = function getAppInfo(record_url) {
+  const matchResult = record_url.match(/\/k\/(\d+)\/show#record=(\d+)/);
+
+  if (matchResult) {
+    const app_id = matchResult[1];
+    const record_id = matchResult[2];
+    console.log("app_id:", app_id);
+    console.log("record_id:", record_id);
+    return { app_id: app_id, record_id: record_id };
+  } else {
+    console.log("URLからapp_idとrecord_idを取得できませんでした。");
+    return {};
+  }
+};
---
+++ src/utils/resistEnrollment.js
@@ -0,0 +1,51 @@
+const OFFISTA_CLASS_PATH = "../class/Offista";
+const MONDETORY_EMPLOYEE_PATH = "../templates/json/mandetory_employee.json";
+const Offista = require(OFFISTA_CLASS_PATH);
+
+module.exports = async function resistEnrollment(record) {
+  let return_obj = { is_successed: true, error_message: "" };
+  try {
+    const company_name = record["会社名"].value;
+    const offista_instance = new Offista({ is_dumpLog: true });
+    const api_key = await offista_instance.get_api_key();
+    let result = await offista_instance.get_consignment_customer(api_key);
+
+    let station_id = "";
+    result.forEach((element) => {
+      if (element.customer_name === company_name)
+        station_id = element.identifier;
+    });
+    if (station_id === "") {
+      console.error(
+        `"${company_name}" is not defined on the office station server.`
+      );
+      return {
+        is_successed: false,
+        error_message: `"${company_name}" is not defined on the office station server.`,
+      };
+    }
+
+    const mondetory_employee_obj = require(MONDETORY_EMPLOYEE_PATH);
+    mondetory_employee_obj.customer_employee_id = record["レコード番号"].value;
+    mondetory_employee_obj.shi_name = record["姓"].value;
+    mondetory_employee_obj.mei_name = record["名"].value;
+    mondetory_employee_obj.birthday = record["生年月日"].value;
+
+    const resist_result = await offista_instance.entry_employee(
+      api_key,
+      station_id,
+      [mondetory_employee_obj]
+    );
+    if (resist_result.is_successed == false) {
+      console.error("failed to entry employee data.");
+      return {
+        is_successed: false,
+        error_message: resist_result.error_message,
+      };
+    }
+    return { is_successed: true, error_message: "" };
+  } catch (e) {
+    console.error(e);
+    return { is_successed: false, error_message: e };
+  }
+};
