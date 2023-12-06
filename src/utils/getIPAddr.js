9145200d222a77526016eee9894e9dbdc7e54b13
kento tokura <k-tokura@nkr-group.com>
2023-12-06 17:23:56 +0900

    adapt to linux machine

--- src/utils/getIPAddr.js
+++ src/utils/getIPAddr.js
@@ -3,9 +3,10 @@ const os = require("os");
 // localhostのIPv4アドレスをstringでreturn
 module.exports = () => {
   const networkInterfaces = os.networkInterfaces();
-
+  // console.log(networkInterfaces);
   // ネットワークインターフェースのキーを調査
   for (const interfaceKey in networkInterfaces) {
+    if (interfaceKey === "lo") continue;
     const interfaceInfo = networkInterfaces[interfaceKey];
 
     // IPv4 アドレスを検索
